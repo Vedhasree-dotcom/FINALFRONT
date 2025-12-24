17959127ffd5c8208984bf86a20889dbb595df22
Vedhasree-dotcom <vedhasatheesh004@gmail.com>
2025-12-24 23:24:10 +0530

    ..

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
-    <footer className="footer">
+    <footer className="footer" >
       <div className="footer-container">
 
         <div className="footer-brand">
